924a3b50804eacad1aae6dffe4194d790b989e79
Natalia Kubiak <nataliia.kubiak@gmail.com>
2023-10-05 16:45:50 +0200

    Update baseTest for running tests in Selenium Grid

--- src/test/java/org/portfolio/tests/BaseTest.java
+++ src/test/java/org/portfolio/tests/BaseTest.java
@@ -1,19 +1,48 @@
 package org.portfolio.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeTest
-    public void setDriver() {
+    public void setDriver() throws MalformedURLException {
+        if(Boolean.getBoolean("selenium.grid.enabled")) {
+            this.driver = getRemoteDriver();
+        } else {
+            this.driver = getLocalDriver();
+        }
+    }
+
+    //if the System Property Variable selenium.grid.enabled = true we will use this driver
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities;
+
+        if(System.getProperty("browser").equalsIgnoreCase("chrome")) {
+            capabilities = new ChromeOptions();
+        } else {
+            capabilities = new FirefoxOptions();
+        }
+
+        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+    }
+
+    //if the System Property Variable selenium.grid.enabled = false we will use this driver
+    private WebDriver getLocalDriver() {
         WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
+        return new ChromeDriver();
     }
 
     @AfterTest
